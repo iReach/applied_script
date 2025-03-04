cc7232dd85e8f14b0d90d6fdf0af465681620789
Erik Ahlroth <erik.ahlroth@hotmail.se>
2025-03-04 13:34:38 +0100

    Ensured test_network_config could be run and added more skeleton code

--- tests/test_network_config.py
+++ tests/test_network_config.py
@@ -7,7 +7,7 @@ from netmiko import ConnectHandler
 
 class TestNetworkConfig:
 
-    def setup(self):
+    def setup_method(self):
         """
         Skapa en instans av NetworkConfigManager.
         Anropa connect() -metoden i instansen för att etablera SSH-anslutningen till
@@ -20,22 +20,39 @@ class TestNetworkConfig:
         prefix.
         """
         self.net_config = NetworkConfigManager()
+        self.net_config.connect()
+        self.net_config.update_hostname("1")
+        self.net_config.update_interface_state("down")
+        self.net_config.update_response_prefix("Standard Response")
 
-    def teardown(self):
+    def teardown_method(self):
         """
         Skapa en teardown_method som ska:
         Stänga SSH-anslutningen efter varje test genom att anropa disconnect()
         """
-        pass
+        self.net_config.disconnect()
 
     def test_show_host_name(self):
         # Tests that the connection returns hostname 1
-        pass
+        hostname = self.net_config.show_hostname()
+        assert hostname == "hostname: 1"
 
     def test_show_interface_state(self):
         # Tests that the connection returns correct interface state
-        pass
+        assert False
 
     def test_show_response_prefix(self):
         # tests that response prefix is good
-        pass
+        assert False
+
+    def test_update_hostname(self):
+        # tests that hostname can be updated from its default values
+        assert False
+
+    def test_update_interface_state(self):
+        # tests that interface state can be updated from its default values
+        assert False
+
+    def test_response_prefix(self):
+        # tests that response prefix can be updated from its default values
+        assert False
