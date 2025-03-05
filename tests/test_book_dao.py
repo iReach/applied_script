6615efef8283da7ba19c1cc9fdc4bb4882e05442
Erik Ahlroth <erik.ahlroth@hotmail.se>
2025-03-05 14:01:04 +0100

    Created alternate version of code using fixtures instead of setup/teardown and checked that tests still passes

--- tests/test_book_dao.py
+++ tests/test_book_dao.py
@@ -1,57 +1,54 @@
 import os
 import sys
+
+import pytest
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 
 from book import Book
 from book_dao import BookDAO
 import sqlite3
 
+
+@pytest.fixture
+def book_dao():
+    book_dao = BookDAO("test_book_database.db")
+    book_dao.insert_book(Book('sommaren utan regn', 'en roman', 'Maggie O farrel' ))
+    book_dao.insert_book(Book('En oväntad vänskap', 'boken baserat på en film', 'Abdel Sellou'))
+    book_dao.insert_book(Book('Förstå och förklara','psykologins vetenskapsteori', 'Helge malmgren'))
+    yield book_dao
+    book_dao.clear_table()
+    book_dao.close()
+
 class TestBookDAO:
-    def setup_method(self):
-        #Setups the database for testing, this includes adding 3 books
-        self.conn = sqlite3.connect('test_book_database.db')
-        self.cursor = self.conn.cursor()
-        self.book_dao = BookDAO("test_book_database.db")
-        self.book_dao.insert_book(Book('sommaren utan regn', 'en roman', 'Maggie O farrel' ))
-        self.book_dao.insert_book(Book('En oväntad vänskap', 'boken baserat på en film', 'Abdel Sellou'))
-        self.book_dao.insert_book(Book('Förstå och förklara','psykologins vetenskapsteori', 'Helge malmgren'))
-
-    def teardown_method(self):
-        self.book_dao.clear_table()
-        self.book_dao.close()
-        self.conn.close()
-
-    def test_get_all_books(self):
+    def test_get_all_books(self, book_dao):
         # Verifies that 3 Books exists in database
-        all_books = self.book_dao.get_all_books()
+        all_books = book_dao.get_all_books()
         assert len(all_books) == 3
         
-
-    def test_add_new_book(self):
+    def test_add_new_book(self, book_dao):
         # Verifies that a new book can be added and that its 4 total
-        assert len(self.book_dao.get_all_books()) == 3
-        self.book_dao.insert_book(Book("Book4","description4","author4"))
-        assert len(self.book_dao.get_all_books()) == 4
+        assert len(book_dao.get_all_books()) == 3
+        book_dao.insert_book(Book("Book4","description4","author4"))
+        assert len(book_dao.get_all_books()) == 4
 
-    def test_get_book_by_title(self):
+    def test_get_book_by_title(self, book_dao):
         # Finds a book by title and verifies that the book is correct
-        temp_book = self.book_dao.find_by_title("sommaren utan regn")
+        temp_book = book_dao.find_by_title("sommaren utan regn")
         assert temp_book.description == 'en roman' 
         
-
-    def test_get_book_and_update(self):
+    def test_get_book_and_update(self, book_dao):
         # Finds a book by title and then changes description
-        temp_book = self.book_dao.find_by_title("sommaren utan regn")
+        temp_book = book_dao.find_by_title("sommaren utan regn")
         temp_book.description = "sommaren med regn"
-        self.book_dao.update_book(temp_book)
-        updated_book = self.book_dao.find_by_title("sommaren utan regn") 
+        book_dao.update_book(temp_book)
+        updated_book = book_dao.find_by_title("sommaren utan regn") 
         assert updated_book.description == "sommaren med regn" 
 
-    def test_get_book_and_delete(self):
+    def test_get_book_and_delete(self, book_dao):
         # Finds a book and them removes it
-        temp_book = self.book_dao.find_by_title("sommaren utan regn")
+        temp_book = book_dao.find_by_title("sommaren utan regn")
         assert temp_book is not None
-        self.book_dao.delete_book(temp_book)
-        deleted_book = self.book_dao.find_by_title("sommaren utan regn")
+        book_dao.delete_book(temp_book)
+        deleted_book = book_dao.find_by_title("sommaren utan regn")
         assert deleted_book == None 
         
--- tests/test_network_config.py
+++ tests/test_network_config.py
@@ -1,58 +1,56 @@
 import os
 import sys
+
+import pytest
 sys.path.append(os.path.join(os.path.dirname(__file__),'../'))
 
 from network_config_manager import NetworkConfigManager
 from netmiko import ConnectHandler
 
+@pytest.fixture
+def net_config():
+    """
+    Skapa en instans av NetworkConfigManager.
+    Anropa connect() -metoden i instansen för att etablera SSH-anslutningen till
+    servern.
+    Återställa alla konfigurationsvärden till utgångsläge genom att anropa rätt
+    metoder i instansen:
+    update_hostname("1") för att sätta hostname till "1".
+    update_interface_state("down") för att sätta interface state till "down".
+    update_response_prefix("Standard Response") för att sätta response
+    prefix.
+    """
+    net_config = NetworkConfigManager()
+    net_config.connect()
+    net_config.update_hostname("1")
+    net_config.update_interface_state("down")
+    net_config.update_response_prefix("Standard Response")
+    yield net_config
+    net_config.disconnect()
+
+
 class TestNetworkConfig:
-
-    def setup_method(self):
-        """
-        Skapa en instans av NetworkConfigManager.
-        Anropa connect() -metoden i instansen för att etablera SSH-anslutningen till
-        servern.
-        Återställa alla konfigurationsvärden till utgångsläge genom att anropa rätt
-        metoder i instansen:
-        update_hostname("1") för att sätta hostname till "1".
-        update_interface_state("down") för att sätta interface state till "down".
-        update_response_prefix("Standard Response") för att sätta response
-        prefix.
-        """
-        self.net_config = NetworkConfigManager()
-        self.net_config.connect()
-        self.net_config.update_hostname("1")
-        self.net_config.update_interface_state("down")
-        self.net_config.update_response_prefix("Standard Response")
-
-    def teardown_method(self):
-        """
-        Skapa en teardown_method som ska:
-        Stänga SSH-anslutningen efter varje test genom att anropa disconnect()
-        """
-        self.net_config.disconnect()
-
-    def test_show_host_name(self):
+    def test_show_host_name(self, net_config):
         # Tests that the connection returns hostname 1
-        hostname = self.net_config.show_hostname()
+        hostname = net_config.show_hostname()
         assert hostname == "hostname: 1"
 
-    def test_show_interface_state(self):
+    def test_show_interface_state(self, net_config):
         # Tests that the connection returns correct interface state
         assert False
 
-    def test_show_response_prefix(self):
+    def test_show_response_prefix(self, net_config):
         # tests that response prefix is good
         assert False
 
-    def test_update_hostname(self):
+    def test_update_hostname(self, net_config):
         # tests that hostname can be updated from its default values
         assert False
 
-    def test_update_interface_state(self):
+    def test_update_interface_state(self, net_config):
         # tests that interface state can be updated from its default values
         assert False
 
-    def test_response_prefix(self):
+    def test_response_prefix(self, net_config):
         # tests that response prefix can be updated from its default values
         assert False
